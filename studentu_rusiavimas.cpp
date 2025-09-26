1b02cda6cf706037a01c85dd972792f1051b4c9a
StaArnas <thisisarnas@gmail.com>
2025-09-26 19:46:15 +0300

    pridetas "meniu"
    meniu leis vartotojui pasirinkti ka jis nori daryti su programa.

--- studentu_rusiavimas.cpp
+++ studentu_rusiavimas.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <numeric>
+#include <limits>
+
 
 using namespace std;
 
@@ -14,3 +18,39 @@ struct Studentas {
 
 };
 
+int main(){
+
+    vector<Studentas> studentai;
+    char pasirinkimas;
+
+    do {
+
+        cout << "\n --- Meniu ---\n"
+             << "1. Ivesti duomenis ranka\n"
+             << "2. Baigti darba\n"
+             << "Jusu pasirinkimas: ";
+        cin >> pasirinkimas;
+
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            pasirinkimas = '0';
+        }
+
+        switch (pasirinkimas) {
+            case '1':
+                cout << "Sekmingai pasirinktas skaicius 1.";
+                break;
+            case '2':
+                cout << "Programa baigia darba.\n";
+                break;
+            default:
+                cout << "Neteisingas pasirinkimas. Bandykite dar karta.\n";
+                break;
+        }
+
+    } while (pasirinkimas != '2');
+
+    return 0;
+
+}
