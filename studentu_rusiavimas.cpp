9dcd1110c990cc835333c22bdcc5977e3efbfebd
StaArnas <thisisarnas@gmail.com>
2025-09-26 19:38:41 +0300

    sukurta studento struktura

---
+++ studentu_rusiavimas.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Studentas {
+
+    string vardas;
+    string pavarde;
+    vector<int> nd;
+    int egzaminas;
+    double galutinisVid;
+    double galutinisMed;
+
+};
+
